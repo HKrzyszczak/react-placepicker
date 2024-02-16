d37189a346277c32b80a95b9cb68718773b41ade
Henryk Krzyszczak <henryk.krzyszczak@intive.com>
2024-02-16 13:05:29 +0100

    fetch using await

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,14 +4,14 @@ import { useEffect } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+    fetchPlaces();
   }, []);
 
   return (
